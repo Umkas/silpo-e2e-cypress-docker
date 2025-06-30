7e8472a2c02ef048bc80a2880afe26e79c2706d8
Dmytro Yerchak <umkauskas@gmail.com>
2025-06-30 18:40:19 +0300

    check for autorun github actions

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    specPattern: 'cypress/e2e/**/*.cy.{js,ts}',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
