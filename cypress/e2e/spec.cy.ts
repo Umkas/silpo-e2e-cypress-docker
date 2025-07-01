ac1ee1c87810d7795147b7172074050ad8759d30
Dmytro Yerchak <umkauskas@gmail.com>
2025-07-01 08:11:51 +0300

    2nd try autorun github actions after setting img public

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -2,4 +2,4 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://example.cypress.io')
   })
-})   // comment for autorun github actions testing вававававава
+})   // comment for autorun github actions testing вававававава 222222
